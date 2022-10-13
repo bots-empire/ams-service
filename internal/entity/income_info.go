74c65bf7064382ef356b5b7ab9179079cca6e273
ChumakovAndrew2002 <smetty6@yandex.ru>
2022-10-13 16:40:56 +0300

    new income_info_route

---
+++ internal/entity/income_info.go
@@ -0,0 +1,27 @@
+package entity
+
+import "fmt"
+
+type IncomeInfo struct {
+	UserID       int64  `json:"user_id,omitempty"`
+	BotLink      string `json:"bot_link,omitempty"`
+	BotName      string `json:"bot_name,omitempty"`
+	IncomeSource string `json:"income_source,omitempty"`
+	TypeBot      string `json:"type_bot,omitempty"`
+}
+
+func (i *IncomeInfo) Validate(userInfo bool) error {
+	if i.UserID == 0 {
+		return fmt.Errorf("user_id is empty")
+	}
+
+	if i.IncomeSource == "" {
+		return fmt.Errorf("source is empty")
+	}
+
+	if i.BotLink == "" {
+		return fmt.Errorf("bot_link is empty")
+	}
+
+	return nil
+}
